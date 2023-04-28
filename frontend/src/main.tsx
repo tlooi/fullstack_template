3fce62a432d1e00460c84631edfe94a497213697
tlooi <t.looi@outlook.com>
2023-04-28 14:52:38 +1200

    removed testing values

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-import { values } from '@shared/test.ts';
-console.log(values);
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <App />
