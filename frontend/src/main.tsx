3e6a1abf7a5402cc6ca82768011febfe76c15116
tlooi <t.looi@outlook.com>
2023-04-28 14:52:22 +1200

    add shared to frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { values } from '@shared/test.ts';
+console.log(values);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
 )
