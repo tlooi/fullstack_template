8594b2b6ebd574682c7a94ce4357232d6b2fe47e
tlooi <t.looi@outlook.com>
2023-04-29 17:48:10 +1200

    fix: missed rename of execution script

--- dev-run.js
+++ dev-run.js
@@ -4,7 +4,7 @@ import { stdout } from 'process';
 exec('cd ./frontend && npm run dev').stdout.on('data', (data) => {
     stdout.write(data);
 });
-exec('cd ./backend && nodemon ./dev-build.js --ignore "build/*"').stdout.on(
+exec('cd ./backend && nodemon ./dev-run.js --ignore "build/*"').stdout.on(
     'data',
     (data) => {
         stdout.write(data);
