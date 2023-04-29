67c4e3707236f58f1da927bec4bee2136c97c704
tlooi <t.looi@outlook.com>
2023-04-29 17:33:34 +1200

    feat: add unified spot to load both dev servers

---
+++ dev-run.js
@@ -0,0 +1,12 @@
+import { exec } from 'child_process';
+import { stdout } from 'process';
+
+exec('cd ./frontend && npm run dev').stdout.on('data', (data) => {
+    stdout.write(data);
+});
+exec('cd ./backend && nodemon ./dev-build.js --ignore "build/*"').stdout.on(
+    'data',
+    (data) => {
+        stdout.write(data);
+    },
+);
